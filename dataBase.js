fd974724b37a9c9ca0846419862c36c183e08911
Nepre <egmtz98@gmail.com>
2018-12-07 17:17:25 +0100

    Guardar RSAPub y Priv

--- dataBase.js
+++ dataBase.js
@@ -19,8 +19,8 @@ function startDataBase() {
     if(optionsJSON.hasOwnProperty('path'))
       document.getElementById("path").value = optionsJSON.path;
 
-      if(optionsJSON.hasOwnProperty('pathCifrado'))
-        document.getElementById("pathCifrado").value = optionsJSON.pathCifrado;
+    if(optionsJSON.hasOwnProperty('pathCifrado'))
+      document.getElementById("pathCifrado").value = optionsJSON.pathCifrado;
   });
 
 }
@@ -70,4 +70,50 @@ function saveDirectory(){
   });
 
 
+}
+
+
+function setRSA(RSAPub, RSAPriv) {
+  var optionsJSON;
+  fs.readFile('./options.json', (err, data) => {
+    optionsJSON = JSON.parse(data);
+    if (err)
+    {
+      printMSG("No se han podido abrir las opciones para obtener la RSA.");
+      return;
+    }
+    optionsJSON = JSON.parse(data);
+
+
+    if(optionsJSON.hasOwnProperty('RSAPub') && optionsJSON.hasOwnProperty('RSAPriv')){
+      optionsJSON.RSAPub = RSAPub;
+      optionsJSON.RSAPriv = RSAPriv;
+
+      fs.writeFile('./options.json', JSON.stringify(optionsJSON, null, 4), (err) => {
+        if(err){
+          printMSG("Error al modificar las opciones.");
+        }
+
+      });
+  });
+}
+
+function getRSA() {
+  var optionsJSON;
+  fs.readFile('./options.json', (err, data) => {
+    optionsJSON = JSON.parse(data);
+    if (err)
+    {
+      printMSG("No se han podido abrir las opciones para obtener la RSA.");
+      return;
+    }
+    optionsJSON = JSON.parse(data);
+    if(optionsJSON.hasOwnProperty('RSAPub') && optionsJSON.hasOwnProperty('RSAPriv')){
+      var RSArray = [optionsJSON.RSAPub, optionsJSON.RSAPriv];
+    }
+    else {
+      printMSG("Hay un problema en el sistema de opciones, vuelva a restaurarlo a base.");
+    }
+    return RSArray;
+  });
 }
